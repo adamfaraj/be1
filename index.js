c2b1ddba10234f65b05623c6b1eca03ea3ce9221
Adam Faraj <adamf@marketamerica.com>
2019-08-19 11:16:27 -0400

    add a user

--- index.js
+++ index.js
@@ -5,17 +5,40 @@ AWS.config.update({region: "us-east-1"});
 const dynamodb = new AWS.DynamoDB();
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const table = "users";
+const tableName = "users";
 
-const params = {
-  TableName: table,
-  Key:{"userId": 1}
+function addUser() {
+  let params = {
+    TableName: tableName,
+    Item: {
+      "userId": 3,
+      "user": 'Adam Faraj'
+    }
+  };
+
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.error('Unable to add item. Error JSON: ', JSON.stringify(err, null, 2))
+    }
+    else {
+      console.log('Succeeded ', JSON.stringify(data, null, 2));
+    }
+  })
 }
 
-docClient.get(params, function(err, data) {
-  if (err) {
-    console.error('Unable to read item. Error JSON: ', JSON.stringify(err, null, 2))
-  } else {
-    console.log('Get item succeeded:', JSON.stringify(data, null, 2));
-  }
-});
+function getUser() {
+  let params = {
+    TableName: tableName,
+    Key:{"userId": 1}
+  };
+
+  docClient.get(params, function(err, data) {
+    if (err) {
+      console.error('Unable to read item. Error JSON: ', JSON.stringify(err, null, 2))
+    } else {
+      console.log('Get item succeeded:', JSON.stringify(data, null, 2));
+    }
+  });
+}
+
+addUser();
